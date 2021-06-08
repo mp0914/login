c6c22c1836d1a160e54ed075dcce47d086d78b1f
matthew perry <mp0914@gmail.com>
2021-06-08 14:59:52 -0700

    added boolean IsLoggedIn

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main()
+bool IsLogginIn ()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    string username, password, un, pw; //comparison strings
+
+    cout << "Enter username: "; cin >> username; //prompts user for username and password.
+    cout << "Enter password: "; cin >> password;
+
+    ifstream read("data\\" + username + ".txt"); //ifstream to read from files
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username && pw == password) //if statement comparing un and pw to user input from prompt
+    {
+        return true; //user logged in
+    }
+    else // this executes when user inputs incorrect password
+    {
+        return false; //user not logged in
+    }
 }
